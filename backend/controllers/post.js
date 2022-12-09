0c0f69d9fe6a49ad2756391f86454f02791467ca
Mandfred GRONDIN <106658617+MandfredGRONDIN@users.noreply.github.com>
2022-12-09 20:29:21 +0100

    update post
    Création de post fonctionne correctement, rajout du controller pour la modification de post

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -3,15 +3,15 @@ const fs = require('fs').promises;
 
 exports.createPost = async (req, res) => {
     try{
-        const postObject = JSON.parse(JSON.stringify(req.body.post));
+        const postObject = JSON.parse(req.body.post);
         delete postObject._id;
         delete postObject.userId;
         const post = new Post({
             ...postObject,
             userId: req.auth.userId,
-            /* imageUrl: `${req.protocol}://${req.get("host")}/images/${
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
                 req.file.filename
-            }` */
+            }` 
         })
         console.log(post)
         await post.save();
@@ -24,9 +24,30 @@ exports.createPost = async (req, res) => {
 
 exports.modifyPost = async (req, res) => {
     try{
-
+        const postObject = req.file ? {
+            ...JSON.parse(req.body.post),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${
+                req.file.filename
+          }`
+        } : {
+            ...req.body
+        }
+        delete postObject.userId
+        let post = await Post.findOne({ _id: req.params.id});
+        if(!post){
+            return res.status(404).json({ message: "Not found"})
+        }
+        if(post.userId != req.auth.userId){
+            return res.status(401).json({message : "Unauthorized"})
+        }
+        if(req.file){
+            const filename = post.imageUrl.split("/images/")[1];
+            await fs.unlink(`images/${filename}`)
+        }
+        await Post.updateOne({ _id: req.params.id},{...postObject, _id: req.params.id})
+        return res.status(201).json({message : "Post modified"})
     } catch (e) {
-        
+        return res.status(500).json({message : "Internal error"})
     }
 }
 
